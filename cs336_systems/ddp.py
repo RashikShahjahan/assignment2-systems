94122b1fc260c1b2c1ff3114779355e0ce7f5b06
RashikShahjahan <rashikshahjahan@protonmail.com>
2026-09-05 15:11:08 -0400

    ddp_overlap_individual_parameters

--- cs336_systems/ddp.py
+++ cs336_systems/ddp.py
@@ -5,29 +5,27 @@ class DDP(torch.nn.Module):
     def __init__(self,module: torch.nn.Module):
         super().__init__()
         self.module = module
+        self.pending_work = []
         for param in module.parameters():
             dist.broadcast(param, 0)
         for buffer in module.buffers():
             dist.broadcast(buffer, 0)
 
+        for param in module.parameters():
+            if param.requires_grad:
+                param.register_post_accumulate_grad_hook(self.hook)
+
+
+
     def forward(self, *args, **kwargs):
         return self.module(*args,**kwargs)
 
+    def hook(self,param: torch.Tensor) -> None:
+        handle = dist.all_reduce(param.grad, op=dist.ReduceOp.AVG, async_op=True)
+        self.pending_work.append(handle)
+
     def finish_gradient_synchronization(self):
-        grads = []
-        params = list(self.module.parameters())
-        for param in params:
-            if param.grad is not None:
-                grads.append(param.grad)
-                
-
-        flattened_grads = torch._utils._flatten_dense_tensors(grads)
-        dist.all_reduce(flattened_grads, op=dist.ReduceOp.AVG)
-
-        synced_grads = torch._utils._unflatten_dense_tensors(
-            flattened_grads,
-            grads,
-        )
-
-        for param, synced_grad in zip(params, synced_grads):
-            param.grad.copy_(synced_grad)
+        for handle in self.pending_work:
+            handle.wait()
+        self.pending_work.clear()
+        
